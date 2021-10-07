88809a32cdd98642466f9013fa8a69a494d23604
Christoffer Gamrath Thomsen <christoffer.thomsen@maersk.com>
2021-10-07 08:17:51 +0200

    working

--- src/test/java/org/example/codingdojo/MoneyTest.java
+++ src/test/java/org/example/codingdojo/MoneyTest.java
@@ -13,6 +13,13 @@ public class MoneyTest {
     }
 
     @Test
+    void francMultiplication() {
+        final var five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
+
+    @Test
     void equality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
@@ -33,6 +40,24 @@ public class MoneyTest {
         public boolean equals(Object obj) {
             final var dollar = (Dollar) obj;
             return amount == dollar.amount;
+        }
+    }
+
+    private static class Franc {
+        private final int amount;
+
+        public Franc(int amount) {
+            this.amount = amount;
+        }
+
+        public Franc times(int multiplier) {
+            return new Franc(amount * multiplier);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            final var franc = (Franc) obj;
+            return amount == franc.amount;
         }
     }
 }
