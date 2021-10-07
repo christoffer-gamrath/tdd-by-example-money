9f7f02a53206861c64ee2952d27add0b0b1ee7cc
Christoffer Gamrath Thomsen <christoffer.thomsen@maersk.com>
2021-10-07 07:44:11 +0200

    working

---
+++ src/test/java/org/example/codingdojo/MoneyTest.java
@@ -0,0 +1,24 @@
+package org.example.codingdojo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MoneyTest {
+    @Test
+    void multiplication() {
+        final var five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+
+    private static class Dollar {
+        int amount = 10;
+
+        public Dollar(int amount) {
+        }
+
+        void times(int multiplier) {
+        }
+    }
+}
