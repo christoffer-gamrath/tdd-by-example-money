25f02452e15ebf739c922ee9d8a54fa932d7df13
Christoffer Gamrath Thomsen <christoffer.thomsen@maersk.com>
2021-10-07 08:48:08 +0200

    working

--- src/test/java/org/example/codingdojo/MoneyTest.java
+++ src/test/java/org/example/codingdojo/MoneyTest.java
@@ -8,8 +8,8 @@ public class MoneyTest {
     @Test
     void multiplication() {
         final Money five = Money.dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
@@ -21,11 +21,11 @@ public class MoneyTest {
 
     @Test
     void equality() {
-        assertTrue(new Dollar(5).equals(new Dollar(5)));
-        assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
         assertTrue(new Franc(5).equals(new Franc(5)));
         assertFalse(new Franc(5).equals(new Franc(6)));
-        assertFalse(new Franc(5).equals(new Dollar(5)));
+        assertFalse(new Franc(5).equals(Money.dollar(5)));
     }
 
     private static abstract class Money {
