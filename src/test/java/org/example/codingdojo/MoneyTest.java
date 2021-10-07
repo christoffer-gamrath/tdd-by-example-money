6d4df15d958b1db16544696bc6bfd801ff3481dc
Christoffer Gamrath Thomsen <christoffer.thomsen@maersk.com>
2021-10-07 08:52:16 +0200

    working

--- src/test/java/org/example/codingdojo/MoneyTest.java
+++ src/test/java/org/example/codingdojo/MoneyTest.java
@@ -15,22 +15,18 @@ public class MoneyTest {
     @Test
     void francMultiplication() {
         final var amount = 5;
-        final var five = franc(amount);
-        assertEquals(franc(10), five.times(2));
-        assertEquals(franc(15), five.times(3));
-    }
-
-    private static Franc franc(int amount) {
-        return new Franc(amount);
+        final var five = Money.franc(amount);
+        assertEquals(Money.franc(10), five.times(2));
+        assertEquals(Money.franc(15), five.times(3));
     }
 
     @Test
     void equality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(franc(5).equals(franc(5)));
-        assertFalse(franc(5).equals(franc(6)));
-        assertFalse(franc(5).equals(Money.dollar(5)));
+        assertTrue(Money.franc(5).equals(Money.franc(5)));
+        assertFalse(Money.franc(5).equals(Money.franc(6)));
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
     private static abstract class Money {
@@ -38,6 +34,10 @@ public class MoneyTest {
 
         public static Money dollar(int amount) {
             return new Dollar(amount);
+        }
+
+        public static Franc franc(int amount) {
+            return new Franc(amount);
         }
 
         public abstract Money times(int multiplier);
