653e3f5b6a831980d842ccc5fc4f6bc7abe26a32
Christoffer Gamrath Thomsen <christoffer.thomsen@maersk.com>
2021-10-09 09:19:55 +0200

    working

--- src/test/java/org/example/codingdojo/MoneyTest.java
+++ src/test/java/org/example/codingdojo/MoneyTest.java
@@ -145,4 +145,25 @@ public class MoneyTest {
             return new Money(amount, to);
         }
     }
+
+    private static class Pair {
+        private final String from;
+        private final String to;
+
+        private Pair(String from, String to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            final var pair = (Pair) obj;
+            return from.equals(pair.from) && to.equals(pair.to);
+        }
+
+        @Override
+        public int hashCode() {
+            return 0;
+        }
+    }
 }
