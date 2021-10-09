9b2f39134fec35a5b70ff5af4f981ffc9c935d5d
Christoffer Gamrath Thomsen <christoffer.thomsen@maersk.com>
2021-10-09 08:04:20 +0200

    working

--- src/test/java/org/example/codingdojo/MoneyTest.java
+++ src/test/java/org/example/codingdojo/MoneyTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MoneyTest {
     @Test
     void multiplication() {
-        final Money five = Money.dollar(5);
+        final var five = Money.dollar(5);
         assertEquals(Money.dollar(10), five.times(2));
         assertEquals(Money.dollar(15), five.times(3));
     }
