879f13e7986152d4885b0ffc7a0726132c42a7e7
Christoffer Gamrath Thomsen <christoffer.thomsen@maersk.com>
2021-10-09 07:24:48 +0200

    working

--- src/test/java/org/example/codingdojo/MoneyTest.java
+++ src/test/java/org/example/codingdojo/MoneyTest.java
@@ -71,7 +71,7 @@ public class MoneyTest {
         }
 
         public Money times(int multiplier) {
-            return Money.dollar(amount * multiplier);
+            return new Dollar(amount * multiplier, "USD");
         }
     }
 
